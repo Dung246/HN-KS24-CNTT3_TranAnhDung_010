08bbeab113a926b2b2a3226e3365272b6a765f1d
Dung246 <dungoo2605@gmail.com>
2026-02-25 15:24:56 +0700

    Update B1.java

--- src/B1/B1.java
+++ src/B1/B1.java
@@ -1,4 +1,39 @@
 package B1;
 
+import java.util.Scanner;
+
 public class B1 {
+    public static void main(String[] args) {
+        System.out.println(" Nhập: ");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int letters = 0;
+        int digits = 0;
+        int special = 0;
+        boolean isPalindrome = true;
+        int n = s.length();
+        for (int i = 0;i< n;i++) {
+            char c = s.charAt(i);
+            if (Character.isLetter(c))
+                letters++;
+            else if (Character.isDigit(c))
+                digits++;
+            else
+                special++;
+
+            if (i < n/2) {
+                if (s.charAt(i) != s.charAt(n-1-i)) {
+                    isPalindrome = false;
+                }
+            }
+
+        }
+        System.out.println("Chữ cái: " + letters);
+        System.out.println("Chữ số: " + digits);
+        System.out.println("Ký tự đặc biệt: " + special);
+        if (isPalindrome)
+            System.out.println("Chuỗi đối xứng");
+        else
+            System.out.println("Không đối xứng");
+    }
 }
