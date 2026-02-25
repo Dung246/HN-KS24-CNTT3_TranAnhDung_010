1778353d4791800a92e8f90f15ec47b3c46eeda6
Dung246 <dungoo2605@gmail.com>
2026-02-25 15:25:38 +0700

    Update B3.java

--- src/B3/B3.java
+++ src/B3/B3.java
@@ -1,4 +1,25 @@
 package B3;
-
+import java.util.Scanner;
 public class B3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        int k = sc.nextInt();
+        boolean found = false;
+        for (int i = 0;i <n && !found;i++) {
+            for (int j =i + 1;j < n;j++) {
+                if (a[i] + a[j]==k) {
+                    System.out.println("Cặp tìm được: " + a[i] + " và " + a[j]);
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        if (!found)
+            System.out.println("Không tồn tại cặp nào");
+    }
 }
